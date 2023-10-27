0833a27817bab0d24ae7911ce664f331c75894a7
Saman Shahbazi <sammm.sh@gmail.com>
2023-10-27 07:40:46 +0200

    add simple server

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// healthcheckHandler writes the status and information about the application
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
+
